1af069b290f6acbf948be30f98b674fabd5d84b9
natwillia <natwillia@hartford.edu>
2025-10-08 16:33:34 -0400

    Change the password to any entry

--- commands.sql
+++ commands.sql
@@ -4,21 +4,28 @@ VALUES
   ('bwayne', 'Bruce', 'Wayne', 'bruce@batman.com');
 
 INSERT INTO websites (site_name, site_url)
-VALUES ('Batman News', 'https://batman-news.com/');
+VALUES
+  ('Batman News', 'https://batman-news.com/');
 
 INSERT INTO accounts (user_id, site_id, password, comment)
-VALUES (11, 11, AES_ENCRYPT('batdance123', @key_str, @init_vector),'News about myself, Batman');
+VALUES
+  (11, 11, AES_ENCRYPT('batdance123', @key_str, @init_vector),'News about myself, Batman');
 
 -- 4: Change the Letterboxd URL associated with a password
 UPDATE websites
 SET site_url = 'https://letterboxd.com/journal/'
 WHERE site_url = 'https://letterboxd.com/';
 
+-- 5: Change the password to any entry
+UPDATE accounts
+SET password = AES_ENCRYPT('starzstar46', @key_str, @init_vector)
+WHERE user_id = 6 AND site_id = 6;
+
 -- 6: Remove a tuple based on a URL for food network
 DELETE FROM accounts
 WHERE site_id = (
-  SELECT site_id FROM websites WHERE site_url = 'https://www.foodnetwork.com/'
-);
+  SELECT site_id FROM websites
+  WHERE site_url = 'https://www.foodnetwork.com/');
 
 -- 7: Remove a tuple based on a password for HBO Max
 DELETE FROM accounts
