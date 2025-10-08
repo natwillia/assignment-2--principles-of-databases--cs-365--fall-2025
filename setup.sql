98fb7334e19bbb7bdb26db7b11aad5ed64ec9ebf
natwillia <natwillia@hartford.edu>
2025-10-08 00:25:47 -0400

    Insert values into each table

--- setup.sql
+++ setup.sql
@@ -10,9 +10,9 @@ SET @init_vector = RANDOM_BYTES(16);
 
 CREATE TABLE IF NOT EXISTS users (
   user_id     INT NOT NULL AUTO_INCREMENT,
+  username    VARCHAR(100)     NOT NULL,
   first_name  VARCHAR(100)     NOT NULL,
   last_name   VARCHAR(100)     NOT NULL,
-  username    VARCHAR(100)     NOT NULL,
   email       VARCHAR(100)     NOT NULL,
 
   PRIMARY KEY (user_id)
@@ -20,8 +20,8 @@ CREATE TABLE IF NOT EXISTS users (
 
 CREATE TABLE IF NOT EXISTS websites (
   site_id    INT               NOT NULL AUTO_INCREMENT,
-  site_url   VARCHAR(255)      NOT NULL,
   site_name  VARCHAR(100)      NOT NULL,
+  site_url   VARCHAR(255)      NOT NULL,
 
   PRIMARY KEY (site_id)
 );
@@ -37,3 +37,42 @@ CREATE TABLE IF NOT EXISTS accounts (
   FOREIGN KEY (user_id) REFERENCES users(user_id),
   FOREIGN KEY (site_id) REFERENCES websites(site_id)
 );
+
+INSERT INTO users (username, first_name, last_name, email)
+VALUES
+  ('eschands', 'Edward', 'Scissorhands', 'ed3@gmail.com'),
+  ('fkrueger', 'Freddy', 'Krueger', 'fredkru10@gmail.com'),
+  ('edrav', 'Eric', 'Draven', 'edraven321@gmail.com'),
+  ('leslion', 'Lestat', 'Lioncourt', 'lestatlion@gmail.com'),
+  ('ferbuell', 'Ferris', 'Bueller', 'fbueller80@gmail.com'),
+  ('tval', 'Tiffany', 'Valentine', 'tiffvalen6@gmail.com'),
+  ('waddams', 'Wednesday', 'Addams', 'wedadd@gmail.com'),
+  ('evtreb', 'Evan', 'Treborn', 'evantre@gmail.com'),
+  ('gnftz', 'Ginger', 'Fitzgerald', 'gingfitz@gmail.com'),
+  ('mmfly', 'Marty', 'McFly', 'mcflymart@gmail.com');
+
+INSERT INTO websites (site_name, site_url)
+VALUES
+  ('Ebay', 'https://www.ebay.com/'),
+  ('MGM Plus', 'https://www.mgmplus.com/'),
+  ('Letterboxd', 'https://letterboxd.com/'),
+  ('Food Network', 'https://www.foodnetwork.com/'),
+  ('Youtube', 'https://www.youtube.com/'),
+  ('Starz', 'https://www.starz.com/'),
+  ('Paramount Plus', 'https://www.paramountplus.com/'),
+  ('HBO Max', 'https://play.hbomax.com/'),
+  ('Poshmark', 'https://poshmark.com/'),
+  ('ThredUp', 'https://www.thredup.com/');
+
+INSERT INTO accounts (user_id, site_id, password, comment)
+VALUES
+  (1, 1, AES_ENCRYPT('ebaypass1', @key_str, @init_vector), 'Ebay login'),
+  (2, 2, AES_ENCRYPT('mgmsecure', @key_str, @init_vector), 'MGM Plus subscription'),
+  (3, 3, AES_ENCRYPT('letterboxd123', @key_str, @init_vector), 'Review movies'),
+  (4, 4, AES_ENCRYPT('foodiepass', @key_str, @init_vector), 'Recipe ideas'),
+  (5, 5, AES_ENCRYPT('yt123', @key_str, @init_vector), 'YouTube login'),
+  (6, 6, AES_ENCRYPT('starzwatch', @key_str, @init_vector), 'Starz login'),
+  (7, 7, AES_ENCRYPT('paramountpw', @key_str, @init_vector), 'Paramount Plus login'),
+  (8, 8, AES_ENCRYPT('hbomaxpw', @key_str, @init_vector), 'HBO Max login'),
+  (9, 9, AES_ENCRYPT('poshstyle', @key_str, @init_vector), 'Poshmark closet'),
+  (10, 10, AES_ENCRYPT('thredup123', @key_str, @init_vector), 'ThredUp resale');
